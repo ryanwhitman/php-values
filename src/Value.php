2fc9b9056141ee4403cac198c1860ef23f2d84d1
Ryan Whitman <ryanawhitman@gmail.com>
2023-02-23 16:48:58 -0500

    Allow for ingestion of other Values

--- src/Value.php
+++ src/Value.php
@@ -14,10 +14,18 @@ abstract class Value
 
     public function __construct($value)
     {
+        // Is the value already an instance of the particular Value being
+        // instantiated? If so, it's already been transformed and validated.
         if ($value instanceof static) {
             $transformedValue = $value->get();
             $value = $value->getOrigValue();
         } else {
+            // Is the value an instance of this abstract Value class? If so,
+            // grab its value and then proceed with transformation / validation.
+            if ($value instanceof self) {
+                $value = $value->get();
+            }
+
             try {
                 $transformedValue = $this->performTransformation($value);
                 $isValid = $this->performValidation($transformedValue);
